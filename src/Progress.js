dd7bd52f485902155906b5e233fce5a8cc7711a7
rishabhp1993 <52542242+rishabhp1993@users.noreply.github.com>
2020-10-19 15:25:50 +0530

    first

--- src/Progress.js
+++ src/Progress.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./progress.css";
+import greencar from "./images/greencar.png";
+import CheckIcon from "@material-ui/icons/Check";
 
 class Progress extends React.Component {
   constructor(props) {
@@ -10,7 +12,34 @@ class Progress extends React.Component {
   }
 
   render() {
-    return <div className="container">dsf</div>;
+    return (
+      <div className="container">
+        <div className="carcontainer">
+          <img className="car" alt="milestone" width="40px" src={greencar} />
+        </div>
+        <div className="emptybar">
+          <div className="bar"></div>
+        </div>
+        <div className="stepscontainer">
+          <div class="step">
+            <div className="stepcirclecomplete">
+              <CheckIcon className="icon" />
+            </div>
+          </div>
+          <div class="step">
+            <div className="stepcirclecomplete">
+              <CheckIcon className="icon" />
+            </div>
+            <p className="steplabel">Step 2</p>
+          </div>
+          <div class="step">
+            <div className="stepcirclecomplete">
+              <CheckIcon className="icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default Progress;
