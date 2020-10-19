eaac029d8dd94b99f73bdff6f1e9b0e3aa070225
rishabhp1993 <52542242+rishabhp1993@users.noreply.github.com>
2020-10-19 10:49:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Progress from "./Progress";
+
+export default function App() {
+  return (
+    <div>
+      <Progress />
+    </div>
+  );
+}
---
+++ src/Progress.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./progress.css";
+
+class Progress extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stagecount: 0
+    };
+  }
+
+  render() {
+    return <div className="container">dsf</div>;
+  }
+}
+export default Progress;
