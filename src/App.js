bb20a9abd1a352ac23bd56d0dda1220c2f4f2d47
rishabhp1993 <52542242+rishabhp1993@users.noreply.github.com>
2020-12-02 13:24:44 +0530

    not imp

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Progress from "./Progress";
 export default function App() {
   return (
     <div>
-      <Progress colorprop="green" steplabelsprop="Step 1,step2,step3,step4" />
+      <Progress colorprop="green" steplabelsprop="Step 1,Step2,Step3,Step4" />
     </div>
   );
 }
